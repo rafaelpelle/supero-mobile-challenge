4e06b5c89f3c9a2e615a157851976a72d9225dc0
Rafael Pelle <rafael.pelle@supero.com.br>
2020-03-17 14:34:22 -0300

    react-native setup

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 100,
+}
